c62c1b2200129ad024623a3401242859df3d20db
martinhq30-hub <martinhq30@gmail.com>
2026-07-04 21:23:31 -0500

    Descripción del cambio

--- src/assaycalc/__init__.py
+++ src/assaycalc/__init__.py
@@ -5,13 +5,20 @@ from assaycalc.core import (
     validar_assay,
     componer_assay,
     guardar_csv,
+    leer_csv_collar,
+    validar_collar,
+    validar_integridad_referencial,
 )
+
 
 __all__ = [
     "componer_assay",
     "guardar_csv",
     "leer_csv_assay",
     "validar_assay",
+    "leer_csv_collar",
+    "validar_collar",
+    "validar_integridad_referencial"
 ]
 
-__version__ = "0.2.0.2"
+__version__ = "0.2.0.3"
